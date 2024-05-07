0afc43054c8e7caaa1f2739eb87c667c8d65d373
Patty Ingatar <108689381+Negao00@users.noreply.github.com>
2024-05-07 12:08:59 -0300

    o

---
+++ main.js
@@ -0,0 +1,5 @@
+const dataAtual = mew Date ();
+let dataObjetivo = prompt("Data final ANO-MES-DIA EXEMPLO: 2024-04-10");
+
+dataObjetivo = new date (dataObjetivo + "T23:59:59");
+let diasQueFaltam = Math.floor
