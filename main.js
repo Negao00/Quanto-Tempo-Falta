aefa9c2dd1d0e9947e95bbe69b8f6d8962eea256
Patty Ingatar <108689381+Negao00@users.noreply.github.com>
2024-05-07 12:25:44 -0300

    gordo

--- main.js
+++ main.js
@@ -1,10 +1,7 @@
 const dataAtual = new Date ();
-let dataObjetivo = prompt("Data final ANO-MES-DIA
-EXEMPLO: 2024-04-10");
+let dataObjetivo = prompt("Data final ANO-MES-DIA EXEMPLO: 2024-04-10");
 
 dataObjetivo = new date (dataObjetivo + "T23:59:59");
-let diasQueFaltam = Math.floor ((dataObjetivo - 
-    dataAtual) / 86400000);
+let diasQueFaltam = Math.floor ((dataObjetivo - dataAtual) / 86400000);
 
-document.querySelector("#dias_restantes").textContent = 
-diasQueFaltam;
+document.querySelector("#dias_restantes").textContent = diasQueFaltam;
